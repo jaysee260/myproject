d671350ee098de8ee4e7539bc1a89f5a5a0258f2
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-07-22 10:26:29 -0400

    added comments

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const config = require('./config').init();
 const { b } = require('./console');
 
+// Leverage the use of NODE_ENV to conditionally set up environment
 
 if (process.env.NODE_ENV === 'production' || config.ENV === 'production') {
 
@@ -14,5 +15,5 @@ if (process.env.NODE_ENV === 'production' || config.ENV === 'production') {
   console.log(b(":: MODE = DEVELOPMENT ::"));
   process.env.NODE_ENV = 'development';
   require('./server.js');
-  
+
 } 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
+// Import configs and console tools
 const config = require('./config').init();
 const { g, y, r } = require('./console');
 
+// Declare global variables
 const ENV = process.env.NODE_ENV || config.ENV || 'development';
 const PORT = config.PORT || process.env.PORT || 3000;
 
+// Import dependencies
 const express = require('express');
 const app = express();
 
+// Set up express middleware
 if (ENV === 'development') {
+  // Use morgan ONLY if in development mode
   const logger = require('morgan');
   app.use(logger('dev'));
 }
@@ -15,6 +20,8 @@ if (ENV === 'development') {
 // Initialize DB Connection
 require('./db')(ENV);
 
+
+///// Routes /////
 app.get('/webpack', (req, res) => {
   res.send('hello from express.js');
 });
@@ -23,6 +30,7 @@ app.get('/*', (req, res) => {
   res.send("Hello from my project's server in progress :)");
 });
 
+// Start server
 app.listen(PORT, () => {
   console.log(y(`:: Server running on port ${PORT} ::`));
 });
