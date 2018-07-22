ad747a3014a8f8a2942aebd03981964159517c21
Juan Carlos Gonzalez <jcgonzalezmolina@gmail.com>
2018-07-22 19:25:55 -0400

    updates

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const ENV = process.env.NODE_ENV || config.ENV || 'development';
 const PORT = process.env.PORT || config.PORT || 3000;
 
 // Import dependencies
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -17,6 +18,8 @@ if (ENV === 'development') {
   app.use(logger('dev'));
 }
 
+// app.use( express.static(__dirname + "../dist") );
+
 // Initialize DB Connection;
 // Pass it ENV variable.
 require('./db')(ENV);
@@ -26,6 +29,10 @@ require('./db')(ENV);
 app.get('/webpack', (req, res) => {
   res.send('hello from express.js');
 });
+
+// app.get('/site', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../", "dist", "index.html"));
+// });
 
 app.get('/*', (req, res) => {
   res.send("Hello from my project's server in progress :)");
